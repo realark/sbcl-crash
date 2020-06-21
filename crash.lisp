d1e179bd7b90713c22de98c987e4bcb873d8cd50
Ark <arktheprogrammer@gmail.com>
2020-06-20 19:53:03 -0700

    repro'd bug

--- crash.lisp
+++ crash.lisp
@@ -1,19 +1,34 @@
 (in-package :cl-user)
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (ql:quickload '(:sdl2-mixer :cffi)))
+  (ql:quickload :cffi)
+  #+cffi
+  (progn
+    #+linux
+    (unless (cffi:list-foreign-libraries)
+      (cffi:load-foreign-library "./libSDL2-2.0.so.0")
+      (cffi:load-foreign-library "./libSDL2_mixer-2.0.so.0"))
+    (ql:quickload '(:sdl2-mixer))))
 
 (defvar *sem* (sb-thread:make-semaphore :count 0))
 (defvar *callback-lock* (sb-thread:make-mutex))
 
 (cffi:defcallback postmix-callback :void ((udata :pointer) (stream :pointer) (len :int))
   (declare (ignore udata stream len))
-  (sb-thread:signal-semaphore *sem*)
-  ;; this will run on the foreign thread callback
-  (format T "~A : in callback waiting on mutex~%" (sb-thread:thread-name sb-thread:*current-thread*))
-  (sb-thread:grab-mutex *callback-lock*)
-  (sb-thread:release-mutex *callback-lock*)
-  (sb-thread:signal-semaphore *sem*)
-  (format T "~A : callback complete.~%" (sb-thread:thread-name sb-thread:*current-thread*)))
+  ;; (sb-thread:signal-semaphore *sem*) ; let other threads know our callback is running
+  ;; (format T "~A : in callback waiting on mutex~%" (sb-thread:thread-name sb-thread:*current-thread*))
+  ;; (loop :for i :from 0 :below 10 :do
+  ;;      (sb-ext:gc :full T))
+  ;; (sb-thread:grab-mutex *callback-lock*)
+  ;; (sb-thread:release-mutex *callback-lock*)
+  ;; (sb-thread:signal-semaphore *sem*)
+  (my-callback)
+  ;; (format T "~A : callback complete.~%" (sb-thread:thread-name sb-thread:*current-thread*))
+  )
+
+(defun my-callback ()
+  ;; (format T "~A : in callback waiting on mutex~%" (sb-thread:thread-name sb-thread:*current-thread*))
+  (gc-churn)
+  )
 
 (defun %set-postmix-callback (callback &optional udata)
   (unwind-protect
@@ -22,22 +37,26 @@
          (sdl2-ffi.functions:mix-set-post-mix callback udata))
     (sdl2-ffi.functions:sdl-unlock-audio-device 1)))
 
-(defun run-crash-demo ()
-  (format T "~A: Starting main lisp on thread~%" (sb-thread:thread-name sb-thread:*current-thread*))
+(defun init-mixer ()
   (sb-thread:grab-mutex *callback-lock*)
   (sdl2-mixer:init)
   (sdl2-mixer:open-audio 44100 :s16sys 2 2048)
   (sdl2-mixer:allocate-channels 2)
-  (%set-postmix-callback (cffi:callback postmix-callback))
+  (%set-postmix-callback (cffi:callback postmix-callback)))
+
+(defun run-crash-demo ()
+  (format T "~A: Starting main lisp on thread~%" (sb-thread:thread-name sb-thread:*current-thread*))
+  (init-mixer)
 
   ;; wait until the callback is active
   (format T "~A: Wait on sem ~A.~%" (sb-thread:thread-name sb-thread:*current-thread*) *sem*)
+  (sb-ext:gc :full T)
   (sb-thread:wait-on-semaphore *sem*)
 
   ;; BUG!!!
   ;; Do a gc while the callback frame is active. Crashes on windows.
   (format T "~A: Doing a full GC.~%" (sb-thread:thread-name sb-thread:*current-thread*))
-  (sb-ext:gc :full T)
+  (gc-churn)
 
   (format T "~A: Cleaning up.~%" (sb-thread:thread-name sb-thread:*current-thread*))
   ;; shouldn't make it here, but if we did let's clean up.
@@ -47,3 +66,8 @@
   (%set-postmix-callback (cffi:null-pointer))
   (loop :while (/= 0 (sdl2-mixer:init 0)) :do
        (sdl2-mixer:quit)))
+
+(defun gc-churn ()
+  ;; (sb-ext:gc :full T) ; weirdly enough, doing an explicit GC avoids a crash
+  (loop :for i :from 0 :below 100 :do
+       (make-array 100000 :initial-element (random 73) :adjustable t)))
